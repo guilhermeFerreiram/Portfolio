bf8b9b0867b89d2b05f4cea010f3dee4eb259e6a
Guilherme Ferreira <guil.ferreiram@gmail.com>
2024-03-17 18:44:41 -0300

    Excluir ação de popup, Atualizar projetos

--- app.js
+++ app.js
@@ -1,21 +1,3 @@
-var dialog = document.querySelector('.popup');
-
-function exibirPopup(nomeProjeto, descricaoProjeto, imagemSrc) {
-    var nomeProjetoElement = document.querySelector('.popup__nomeprojeto');
-    var descricaoProjetoElement = document.querySelector('.popup_descricao');
-    var imagemElement = document.getElementById('imagem-popup');
-
-    nomeProjetoElement.textContent = nomeProjeto;
-    descricaoProjetoElement.textContent = descricaoProjeto;
-    imagemElement.src = imagemSrc;
-
-    dialog.showModal();
-}
-
-function fecharPopup() {
-    dialog.close();
-}
-
 function menuShow() {
     let menuMobile = document.querySelector('.mobile__nav');
     if (menuMobile.classList.contains('open')){
