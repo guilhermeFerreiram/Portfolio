4c6ff9bd07386f063acedbad2e35299f44fd11c4
Guilherme Ferreira <guil.ferreiram@gmail.com>
2024-03-17 10:10:10 -0300

    Montar Dinâminca de Exibição dos Projetos

--- app.js
+++ app.js
@@ -20,8 +20,16 @@ function menuShow() {
     let menuMobile = document.querySelector('.mobile__nav');
     if (menuMobile.classList.contains('open')){
         menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "Imagens/icone_menu.png"
     }
     else{
         menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "Imagens/icone_menu_fechar.png"
     }
 }
+
+window.onload = function() {
+    setTimeout(function() {
+        document.getElementById("radio2").checked = true;
+    }, 2000);
+};
